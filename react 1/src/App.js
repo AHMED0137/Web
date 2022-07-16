c24e614838872b6aa5d5e9201f9e3c681c3f8430
AHMED0137 <ch.ahmad0137@gmail.com>
2022-07-16 17:53:31 +0500

    React task
    complete

---
+++ react 1/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+ import './App.css';
+import Nav from './components/nav';
+import Main from './components/main';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+      <hr></hr>
+    </div>
+  )
+}
+export default App;
